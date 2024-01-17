ae94f54382f8f5bb1e38c4950e614dadd3e71e36
Petar Mitchev <pmitchev@Petars-MacBook-Air.local>
2024-01-17 14:58:52 +0200

    Initial commit

---
+++ scripts/leaflet-color-markers.js
@@ -0,0 +1,54 @@
+var bit00000001Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bitgrey.png',
+	iconSize: [32, 39],
+	iconAnchor: [16, 39],
+	popupAnchor: [1, -39]
+});
+var bit0001Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bit-0001.png',
+	iconSize: [30, 37],
+	iconAnchor: [15, 37],
+	popupAnchor: [1, -37]
+});
+var bit001Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bit-001.png',
+	iconSize: [30, 37],
+	iconAnchor: [15, 37],
+	popupAnchor: [1, -37]
+});
+var bit01Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bit-01.png',
+	iconSize: [30, 37],
+	iconAnchor: [15, 37],
+	popupAnchor: [1, -37]
+});
+var bit1Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bit-1.png',
+	iconSize: [30, 37],
+	iconAnchor: [15, 37],
+	popupAnchor: [1, -37]
+});
+var bit10Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bit-10.png',
+	iconSize: [39, 47],
+	iconAnchor: [20, 47],
+	popupAnchor: [1, -47],
+});
+var bit100Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bit-100.png',
+	iconSize: [46, 55],
+	iconAnchor: [23, 55],
+	popupAnchor: [1, -55]
+});
+var bit1000Icon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bit-1000.png',
+	iconSize: [53, 64],
+	iconAnchor: [26, 64],
+	popupAnchor: [1, -64]
+});
+var bitWhiteIcon = new L.Icon({
+	iconUrl: '/assets/marker-icon-2x-bitwhite.png',
+	iconSize: [32, 39],
+	iconAnchor: [16, 39],
+	popupAnchor: [1, -39]
+});
