726194644ae53bbe7553e4e528974faf71844c13
Petar Mitchev <pmitchev@Petars-MacBook-Air.local>
2024-02-09 09:24:14 +0200

    better time ago

--- scripts/maplocks.js
+++ scripts/maplocks.js
@@ -12,27 +12,27 @@ const timeAgo = (date) => {
   const seconds = Math.floor((new Date() - date) / 1000);
 
   let interval = Math.floor(seconds / 31536000);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + ' years ago';
   }
 
   interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + ' months ago';
   }
 
   interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + ' days ago';
   }
 
   interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + ' hours ago';
   }
 
   interval = Math.floor(seconds / 60);
-  if (interval > 1) {
+  if (interval > 0) {
     return interval + ' minutes ago';
   }
 
